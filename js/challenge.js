1a1b509f7964cff4945e9b68982343ca59c75270
Christopher Terrano <mterrano1@yahoo.com>
2021-12-08 13:26:03 -0600

    Done.

---
+++ js/challenge.js
@@ -0,0 +1,49 @@
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const h1 = document.querySelector('#counter');
+//     let num = parseInt(h1.innerText);
+    
+//     function startCounting() {
+//         let counter = parseInt(num);
+//         setInterval(() => {
+//             counter++
+//             h1.innerText = counter
+//         }, 1000);
+//     }
+    
+//     startCounting();
+// })
+
+const h1 = document.querySelector('#counter');
+let num = parseInt(h1.innerText);
+const pauseResumeButton = document.querySelector('#pause');
+const addButton = document.querySelector('#plus');
+const minusButton = document.querySelector('#minus');
+const likeButton = document.querySelector('#heart');
+
+function configureTimer() {
+    let counter = parseInt(num);
+    let startCounter = setInterval(() => {
+        counter++
+        h1.innerText = counter
+    }, 1000);
+    addButton.addEventListener('click', function() {
+        ++counter;
+        return
+    });
+    pauseResumeButton.addEventListener('click', function(){
+        if (pauseResumeButton.textContent === ' pause '){
+        clearInterval(startCounter),
+        pauseResumeButton.textContent = ' resume '
+    }else {startCounter}
+    })
+}
+
+// addButton.addEventListener('click', function() {
+//     ++counter;
+//     return
+// })
+
+configureTimer();
+
